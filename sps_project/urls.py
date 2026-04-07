cef37bb348083f965c67fd405ec0b83fe679da5d
lopatin.maxim <lopatin.maxim2012@yandex.ru>
2026-04-07 20:48:32 +0300

    Feature: 1) Добавлено базовое логирование в систему.

--- sps_project/urls.py
+++ sps_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
 
 
 from sps_project.settings import DEBUG
@@ -11,6 +12,18 @@ from sps_project.settings import MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sps_dashboard.urls')),
+
+    # Встроенная авторизация.
+    path('login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html',
+        redirect_authenticated_user=True,
+    ), name='login'),
+
+    # Выход из системы.
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='sps_dashboard:line_list',
+    ), name='logout'),
+
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
 
 if DEBUG:
